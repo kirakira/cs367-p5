82544376b3930b6ee05d47f202780999c8ef799c
Menghui Wang <wmhkebe@gmail.com>
2013-12-03 00:58:08 -0600

    change skeleton to handle exception

--- files/WordFrequencyMain.java
+++ files/WordFrequencyMain.java
@@ -2,18 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class WordFrequencyMain {
-    public static final void main(String[] args) throws FileNotFoundException, IOException {
+    public static final void main(String[] args) {
         // TODO add your variables
 
-        BufferedReader br = new BufferedReader(new FileReader(args[0]));
-        String line;
-        while ((line = br.readLine()) != null) {
-            String[] words = line.split(" ");
-            for (String word: words) {
-                // TODO update your map accordingly
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(args[0]));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String word: words) {
+                    // TODO update your map accordingly
+                }
             }
+            br.close();
+        } catch (FileNotFoundException e) {
+            // TODO handle exception here
+        } catch (IOException e) {
+            // TODO handle exception here
         }
-        br.close();
 
         // TODO process and output the top 11 most frequent word
     }
